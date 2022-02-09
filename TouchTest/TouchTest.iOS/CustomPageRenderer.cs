e8f06c9aebe184e5a0c87cd3aaf5e1351adfa5c4
ThomasAdrian <33392738+ThomasAdrian@users.noreply.github.com>
2022-02-09 14:25:20 +0000

    Add project files.

---
+++ TouchTest/TouchTest.iOS/CustomPageRenderer.cs
@@ -0,0 +1,21 @@
+﻿using TouchTest.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+//uncomment below for workaround ie disabling navigation pop gesture
+//[assembly: ExportRenderer(typeof(Page), typeof(CustomPageRenderer))]
+namespace TouchTest.iOS
+{
+    class CustomPageRenderer : PageRenderer
+    {
+        public override void ViewWillLayoutSubviews()
+        {
+            base.ViewWillLayoutSubviews();
+
+            if (NavigationController != null)
+            {
+                NavigationController.InteractivePopGestureRecognizer.Enabled = false;
+            }
+        }
+    }
+}
---
+++ TouchTest/TouchTest/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TouchTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            //with the page in the navigation stack the toucheffect kills other page gestures
+            //when done from the left hand side
+            await Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
